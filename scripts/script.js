1779daac37e5e701af2e37e85e859aee299be500
Sergey Shalnev <M89859908253@yandex.ru>
2020-10-31 02:22:57 +0300

    rew

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,31 @@
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = popup.querySelector('.popup__close-button');
-//const saveButton = popup.querySelector('.popup__save-button');
+const forma = document.querySelector('.popup__form');
+const profileName = document.querySelector('.profile__name');
+const discription = document.querySelector('.profile__discription');
+const inputName = document.querySelector('.popup__input_name');
+const inputDiscription = document.querySelector('.popup__input_discription');
 
 function showPopup(){
-  popup.classList.add('popup__opend')
+  popup.classList.add('popup_opened')
+inputName.value = profileName.textContent;
+inputDiscription.value = discription.textContent;
+
 }
 function closePopup(){
-  popup.classList.remove('popup__opend')
+  popup.classList.remove('popup_opened')
 }
 editButton.addEventListener('click', showPopup);
 closeButton.addEventListener('click', closePopup);
 
-const forma = document.querySelector('.popup__form');
-const profileName = document.querySelector('.profile__name');
-const discription = document.querySelector('.profile__discription');
-const inputName = document.querySelector('.popup__input-name');
-const inputDiscription = document.querySelector('.popup__input-discription');
+
 
 function submitForm(e) {
   e.preventDefault();
   profileName.textContent = inputName.value;
   discription.textContent = inputDiscription.value;
+  closePopup();
 }
 forma.addEventListener('submit', submitForm);
-//saveButton.addEventListener('click', closePopup);
 forma.addEventListener('submit', closePopup);
